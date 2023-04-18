76a7c8c8fa6b74ba504b10f943f258afbd68dbb7
Ivan_Peskov <ipeskov@gmail.com>
2023-04-18 13:03:08 +0300

    Otvet_2.1

--- Zadacha_2/Otvet/Program.cs
+++ Zadacha_2/Otvet/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
 // в промежутке от M до N. C помощью рекурсии.
 
+Console.Write (" Введите число М-");
+int m = Conver.ToInt32 ( Console.ReadLine ());
+
+Console.Write (" Введите число N-");
+int n = Conver.ToInt32 ( Console.ReadLine ());
+
+void Sum_M_N ( int m, int n );
+{}
