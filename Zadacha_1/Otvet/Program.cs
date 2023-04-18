c2dcc1a23764cd70984b6b430cf47237b63ec642
Ivan_Peskov <ipeskov@gmail.com>
2023-04-18 12:47:42 +0300

    Otvet_1.2

--- Zadacha_1/Otvet/Program.cs
+++ Zadacha_1/Otvet/Program.cs
@@ -1,2 +1,6 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все четные натуральные числа 
 // в промежутке от М до N с помощью рекурсии.
+Console.Write ("Введите значение М-");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите значение N-");
+int n = Convert.ToInt32 (Console.ReadLine());
