c504aebc40f8031f60e6a6c8caf062d4f8e64de6
Ivan_Peskov <ipeskov@gmail.com>
2023-04-18 13:07:38 +0300

    Otvet_1.4

--- Zadacha_1/Otvet/Program.cs
+++ Zadacha_1/Otvet/Program.cs
@@ -3,8 +3,10 @@
 
 Console.Write ("Введите значение М-");
 int m = Convert.ToInt32 (Console.ReadLine());
+
 Console.Write ("Введите значение N-");
 int n = Convert.ToInt32 (Console.ReadLine());
+
 void che (int m, int n)
 {
     if (m > n)
